2be5a93d8ebad1596a0c7d63d38f16ca139a1e0c
SheikhAKarim <sheikhakarim25@gmail.com>
2017-07-10 05:34:15 +0500

    Moving App to Git

---
+++ OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "fakebsef14@gmail.com";
+        public string MailFromAddress = "fakebsef14@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "fakebsef14@gmail.com";
+        public string Password = "secretpassword";
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
